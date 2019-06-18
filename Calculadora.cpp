e4166979aba9cbfa62c92c5bb0c80c8fdcdd6382
Carlos <c_salazarmartinez@hotmail.com>
2019-06-18 14:55:03 -0500

    Codigo_Base
    Se agregó el archivo principal con el menú

---
+++ Calculadora.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+   char opcion;
+
+   cout << "Elija la operacion que desea realizar: \n a) Seno \n b) Coseno \n c )Tangente \n d )Arco seno \n e) Arco coseno \n f) Arco Tangente \n";
+
+   cin >> opcion;
+
+
+   switch(opcion)
+   {
+         case 'a':
+            break;
+         case 'b':
+            break;
+         case 'c':
+            break;
+         case 'd':
+            break;
+         case 'e':
+            break;
+         case 'f':
+            break;
+
+         default:
+            cout << "Elija una opcion correcta";
+   }
+
+}
