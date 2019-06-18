d6c076205ffde416fbdc80087514f135c2cef7b1
Sergio <s.nunezsantoyo@ugto.mx>
2019-06-18 11:26:56 -0500

    Codigo Coseno y Seno

---
+++ SenoCoseno.cpp
@@ -0,0 +1,22 @@
+
+float funcionSeno(float Entrada);
+
+float funcionSeno(Entrada){
+	float Salida;
+	
+	Salida =  sin(Entrada);
+	
+	return Salida;
+}
+
+float funcionCoseno(float Entrada);
+
+float funcionCoseno(Entrada){
+	float Salida;
+	
+	Salida =  cos(Entrada);
+	
+	return Salida;
+}
+
+
