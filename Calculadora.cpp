8371f89d7335723beb3344dcbded94afade781b4
Victoria <martinez15zz@live.com.mx>
2019-06-18 09:05:48 -0500

    Initial commit

---
+++ Calculadora.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string.h>
+#include<math.h>
+
+using namespace std;
+int main(){
+    cout << "Introduzca un numero \n";
+    float numero;
+    cin >> numero
+    
+cout << "Seleccione una operacion \n 1 Seno \n 2 Coseno \n 3 Tangente \n 4 Arco Seno \n 5 Arco Coseno \n 6 Arco Tangente \n ";
+ return 0 ;
+
+ 
+}
